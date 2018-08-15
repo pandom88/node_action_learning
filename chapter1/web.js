b28acb81dce8594c7170f49c20d1d213f647cf55
pandom88 <pandom@outlook.com>
2018-08-15 23:27:26 +0800

    chapter1

---
+++ chapter1/web.js
@@ -0,0 +1,14 @@
+let http = require('http');
+
+function process_request(req,res){
+    const body = 'Thanks for 的!\n';
+    let content_length = body.length;
+    res.writeHead(200,{
+        'Content-Length':content_length,
+        'Content-Type':'text/plain'
+    });
+    res.end(body);
+}
+
+let s = http.createServer(process_request);
+s.listen(8080);
